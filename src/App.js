96a971257c4f994d8cc2d07d70a64f33699086d9
Antonio Rivera <antoniorm80@hotmail.com>
2020-09-22 17:35:43 -0500

    Agregando el PageHeader

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import { CssBaseline, makeStyles } from '@material-ui/core';
+import { createMuiTheme, CssBaseline, makeStyles } from '@material-ui/core';
 import './App.css';
 import Menu from "./Layout/Menu";
 import Header from "./Layout/Header";
+import PageHeader from "./Layout/PageHeader";
+import PeopleOutlineOutlinedIcon from '@material-ui/icons/PeopleOutlineOutlined';
+
+
+const theme = createMuiTheme({
+   palette: {
+     primary: {
+       main: '#3f51b5',
+       light: '#7986cb'
+     },
+     secondary: {
+      main: '#f50057',
+      light: '#ff4081'
+     },
+     background: {
+       default: '#f4f5fd'
+     }
+   },
+   shape: {
+    borderRadius: '12px'
+   }
+})
 
 const Styles = makeStyles({
   appMain: {
     paddingLeft: '250px',
     width: '100%',
-    //height: '500px',
-    //backgroundColor: "green",
-    //marginRight: '50px',
-    
   }
 })
 
@@ -25,6 +43,12 @@ function App() {
       <Menu/>
       <div className={classes.appMain}> 
           <Header/>
+          <PageHeader
+            title={"Page Header"}
+            subTitle={"Page Description"}
+            icon={<PeopleOutlineOutlinedIcon fontSize="large" />}
+          />
+          <br/>
           Este es el contenido donde irán los controles...
       </div>
       <CssBaseline/>
